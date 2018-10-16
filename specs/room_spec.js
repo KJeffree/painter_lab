9f94bcf95c4bb111c1a4aca3af0fb5a716bd078f
Katie Jeffree <k.jeffree@gmail.com>
2018-10-16 16:47:20 +0100

    finished MVP

--- specs/room_spec.js
+++ specs/room_spec.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const Room = require('../room.js');
 const Paint = require('../paint.js');
+const Decorator = require('../decorator.js');
 
 describe('Room', function () {
 
@@ -43,6 +44,59 @@ describe('Paint', function() {
   it('should be able to empty itself', function(){
     paint.paintWall(1);
     assert.equal(paint.litres, 1)
+  });
+
+});
+
+describe('Decorator', function() {
+  let decorator;
+  let paint;
+
+  beforeEach(function() {
+    decorator = new Decorator();
+    paint1 = new Paint(2);
+    paint2 = new Paint(3);
+    paint3 = new Paint(5);
+    room1 = new Room(10);
+  });
+
+  it('should start with an empty paint stock', function(){
+    assert.deepStrictEqual(decorator.stock, []);
+  });
+
+  it('should be able to add a paint to stock', function(){
+    decorator.addPaintToStock(paint1.litres);
+    assert.deepStrictEqual(decorator.stock, [2]);
+  });
+
+  it('should be able to calculate the total number of litres in stock', function(){
+    decorator.addPaintToStock(paint1.litres);
+    decorator.addPaintToStock(paint2.litres);
+    assert.equal(decorator.findTotal(), 5);
+  });
+
+  it('should be able to calculate if can paint room', function() {
+    decorator.addPaintToStock(paint1.litres);
+    decorator.addPaintToStock(paint2.litres);
+    decorator.addPaintToStock(paint3.litres);
+    assert.equal(decorator.canPaintRoom(room1),true)
+
+  });
+
+  it("should be able to calculate if can't paint room", function() {
+    decorator.addPaintToStock(paint1.litres);
+    decorator.addPaintToStock(paint2.litres);
+    assert.equal(decorator.canPaintRoom(room1),false)
+
+  });
+
+  it("should be able to paint room if enough paint", function(){
+    decorator.addPaintToStock(paint1.litres);
+    decorator.addPaintToStock(paint2.litres);
+    decorator.addPaintToStock(paint3.litres);
+    decorator.addPaintToStock(paint1.litres);
+    decorator.paintRoom(room1);
+    assert.equal(room1.painted,true);
   });
 
 });
